837e06bb78c5e6b476a45f8226cddd725858cc58
FIAN.F2 <alfianabu87@gmail.com>
2025-08-30 13:41:35 +0700

    Add files via upload

---
+++ js/generators/color.js
@@ -0,0 +1,11 @@
+export function generateRandomColor() {
+  // Random 0-255
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+  const hex = `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+
+  return { rgb, hex };
+}
